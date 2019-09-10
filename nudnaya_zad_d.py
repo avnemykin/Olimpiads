24326605077e6f4d3b60ea143328b520f4b9ff1b
avnemykin <55104614+avnemykin@users.noreply.github.com>
2019-09-10 14:31:00 +0700

    Update nudnaya_zad_d.py

--- nudnaya_zad_d.py
+++ nudnaya_zad_d.py
@@ -1,22 +1,18 @@
+from math import sqrt
+with open('input.txt', 'r') as f_in:
+    a,b,c,d = map(int,f_in.readlines())
+
+korny = []
+
 with open('output.txt', 'w') as f_out:
-    s=''
-    if d!=0:
-        for i in range(0,1001,d):
-            if a*(i**3)+b*(i**2)+c*i+d==0:
-                s+=str(i)+' '
-    else:
-        korny.append(0)
-        discr = b**2-4*a*c
-        if discr > 0:
-            if 0<=int((-b-sqrt(discr))/(2*a))<=1000 :
-                korny.append(int((-b-sqrt(discr))/(2*a)))
-            if 0<=int((-b+sqrt(discr))/(2*a))<=1000:
-                korny.append(int((-b+sqrt(discr))/(2*a)))
-                
-        elif discr==0:
-            if 0<=int((-b)/(2*a))<=1000:
-                korny.append(int((-b)/(2*a)))
+    
+
+    for i in range(0,1001,1):
+        if a*(i**3)+b*(i**2)+c*i+d==0:
+             korny.append(i)
+    
     korny.sort()
     korny = map(str,korny)
-    s = " ".join(korny)
-    f_out.write(s)
+    
+    f_out.write(" ".join(korny))
+        
