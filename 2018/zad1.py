925bb63ebfe7cb0959ff02d3e4a1aec23690ba65
avnemykin <55104614+avnemykin@users.noreply.github.com>
2019-09-25 21:13:25 +0700

    Update zad1.py

--- 2018/zad1.py
+++ 2018/zad1.py
@@ -1,1 +1,22 @@
 
+with open("input.txt","r") as f:
+    a = int(f.readline())
+
+
+with open("output.txt","w") as f:
+    summ = a//10 + a%10
+    res = a /summ
+    n = str(res).find('.')
+    drob = str(res)[n+1:]
+    
+
+
+
+    if int(drob) == 0:
+        f.write(str(int(res)))
+    elif len(drob)==1:
+        f.write("{:3.1f}".format(res))
+    elif len(drob)==2:
+        f.write("{:3.2f}".format(res))
+    else:
+        f.write("{:3.3f}".format(res))
